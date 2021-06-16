6f6974168e8432abf877bc44bfa56a79088b8d3c
cardozotiago <cardozotiago@hotmail.com>
2021-06-15 21:09:55 -0300

    #Primeira versão Rest com Jersey no projeto loja.

---
+++ loja/src/test/java/br/com/alura/loja/ProjetoTest.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.alura.loja.modelo.Projeto;
+import junit.framework.Assert;
+
+/**
+ * @author tiago
+ * @date 2021-05-11
+ */
+public class ProjetoTest {
+	private HttpServer server;
+	private Client client;
+
+	@Before
+	public void startaServidor() {
+		this.server = Servidor.inicializaServidor();
+		client = ClientBuilder.newClient();
+	}
+
+	@After
+	public void mataServidor() {
+		server.stop();
+	}
+
+	@Test
+	public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+		WebTarget target = client.target("http://localhost:8080");
+		Projeto projeto = target.path("/projetos/1").request().get(Projeto.class);
+		Assert.assertEquals(1L, projeto.getId(), 0);
+	}
+
+	@Test
+	public void testaQueBuscarUmProjetoTrasUmProjeto() {
+		WebTarget target = client.target("http://localhost:8080");
+		Projeto projeto = target.path("/projetos/1").request().get(Projeto.class);
+		Assert.assertEquals("Minha loja", projeto.getNome());
+	}
+}
